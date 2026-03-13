723b3c1f7f7314a39e284cf8e417c8a61ce885dc
Namitamy0503 <namitami162@gmail.com>
2026-03-13 15:22:37 +0900

    プロフィール画像登録成功

--- lib/features/profile/profile_service.dart
+++ lib/features/profile/profile_service.dart
@@ -4,18 +4,23 @@ import "package:firebase_storage/firebase_storage.dart";
 class ProfileService {
   final FirebaseStorage _storage = FirebaseStorage.instance;
 
-  Future<String> uploadProfileImage(String uid,File image) async{
+  Future<String> uploadProfileImage(String uid, File image) async {
+    try {
+      final timestamp = DateTime.now().millisecondsSinceEpoch;
+      final ref = _storage
+          .ref()
+          .child("users")
+          .child(uid)
+          .child("profile_$timestamp.jpg");
 
-    final timestamp = DateTime.now().millisecondsSinceEpoch;
-    final ref = _storage
-               .ref()
-               .child("users")
-               .child(uid)
-               .child("profile_$timestamp.jpg");
+      // アップロード完了を待機
+      await ref.putFile(image);
 
-    await ref.putFile(image);
-
-    final downloadUrl = await ref.getDownloadURL();
-    return downloadUrl;
+      // 保存されたファイルのURLを取得
+      return await ref.getDownloadURL();
+    } catch (e) {
+      // 呼び出し元の画面でエラーを検知できるように再送
+      rethrow;
+    }
   }
 }
